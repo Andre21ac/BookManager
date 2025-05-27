af4b2abe4f375146f4750215eb9fc84cab679604
André <Andr@DESKTOP-IMK5U7G>
2025-05-27 09:17:35 -0300

    Add project files.

---
+++ BookManagerWinFormsApp/Infraestructure/SqliteDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BookManagerConsoleApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagerConsoleApp.Infraestructure
+{
+    public class SqliteDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbPath = Path.Combine(AppContext.BaseDirectory, "DataBase.db");
+
+            optionsBuilder.UseSqlite(dbPath);
+        }
+    }
+}
